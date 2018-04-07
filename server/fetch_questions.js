475c4158ae21085101ae1cd046ae7abcbce3ec23
Richard Nolan <therichardnolan@gmail.com>
2018-04-07 01:09:11 +0100

    optimized fetch module and added options as parameter

--- server/fetch_questions.js
+++ server/fetch_questions.js
@@ -3,19 +3,73 @@ const randomize = (a, b)=> 0.5 - Math.random(); //positive or negative with 0,1 
 
 // added options parameter with default value of a blank object
 module.exports = (options={})=>{
+    const root = "https://opentdb.com/api.php?";
 
-    return fetch("https://opentdb.com/api.php?amount=10")
+    // options.amount = options.amount || 10; // set default of 10 questions
+    // another way to set defaults but a better method if there is more
+    options = Object.assign({amount:3}, options); 
+
+    // start with blank array
+    let qs = [];
+    for(let option in options){
+        // for each key/value pair add a "key=value" string to the array
+        qs.push( option+"="+options[option] );
+    }
+    // join the array with "&" and combine with root to construct a url and query string
+    let url = root+qs.join("&");
+    
+    console.log("Request made for: "+url)
+    
+    return fetch(url)
+        // format returned data as json
         .then(   res=> res.json()    )
-        // .then(   function(ara){ console.log(ara.results)  }   )
+        // return only the results, stripping the superflous metadata
         .then( res=> res.results  )
-        .then( questions=>{   
+        // deal with the questions
+        .then( questions=>{  
+            
+            // 1, 2, 3, 4 return exactly the same result
+            // They are only ordered by the best most modern method first
+            
+            // 1.
+            return questions.map(q=>{
+                // create a possible_answers key 
+                // create a new array using a spread of the incorrect_answers and the correct answer
+                // then use our randomize sort function from above to shuffle the array 
+                q.possible_answers = [...q.incorrect_answers, q.correct_answer].sort(randomize);
+                return q;
+            })
+
+
+
+            // 2.
+            for(let q of questions){
+                // create a possible_answers key 
+                // create a new array using a spread of the incorrect_answers and the correct answer
+                // then use our randomize sort function from above to shuffle the array 
+                q.possible_answers = [...q.incorrect_answers, q.correct_answer].sort(randomize);
+            } 
+            return questions;
+
+
+            // 3.
+            for(let q of questions){
+                q.possible_answers = q.incorrect_answers.slice();
+                q.possible_answers.push(q.correct_answer);
+                q.possible_answers.sort(randomize); 
+            } 
+            return questions;
+
+            // 4.
             for(let q of questions){
                 let possible_answers = q.incorrect_answers.slice();
                 possible_answers.push(q.correct_answer);
-                // scramble the answers
                 possible_answers.sort(randomize); 
                 q.possible_answers = possible_answers;
             } 
             return questions;
         });
-}
+
+
+
+};
